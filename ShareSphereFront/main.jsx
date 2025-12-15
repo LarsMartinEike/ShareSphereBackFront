84fd895a1fb6220d1f794ee4d867dac886982308
Lars Eike <larsmcg@gmx.de>
2025-12-15 11:16:30 +0100

    Monorep init

---
+++ ShareSphereFront/main.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+const rootEl = document.getElementById('root')
+if (!rootEl) throw new Error("Root-Element mit id='root' wurde nicht gefunden.")
+
+ReactDOM.createRoot(rootEl).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
